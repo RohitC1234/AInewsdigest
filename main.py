a424614bf91e2ca1a97f7da616e78e2a83676c37
Rohit <rohitcharmode2590@gmail.com>
2026-04-06 06:00:03 +0530

    App4 :Getting AI News Summary and Analysis

--- main.py
+++ main.py
@@ -1,14 +1,40 @@
+import requests
+from send_email import send_email
 from langchain.chat_models import init_chat_model
 
+api_key = "7ba478afda5c4e39ba0aa943c37b813c"
 GOOGLE_API_KEY = "AIzaSyDXhS0hCNr7ohiSNElro7WBzVXV6xacw0M"
 
+url = (
+    "https://newsapi.org/v2//top-headlines?"
+    "category=business&"
+    "language=en&"
+    "pageSize=8&"
+    "sortBy=publishedAt&apiKey=" + api_key
+)
+request = requests.get(url)
+content = request.json()
+articles = content["articles"]
+print(articles)
+
 model = init_chat_model(
-    model="gemini-3-flash-preview",
+    model="gemini-2.5-flash",
     model_provider="google-genai",
     api_key=GOOGLE_API_KEY
 )
 
-response = model.invoke("Is a pen better than a pencil?")
-response_str = response.content[0]['text']
+prompt = """
+You're a news summarizer.
+Write a short paragraph analyzing those news.
+Add another second paragraph and tell me 
+how they affect the stock market.
+Summarize the following news articles:
+{articles}
+"""
+
+response = model.invoke(prompt)
+response_str = response.content
 print(response_str)
 
+#body = body.encode("utf-8")
+#send_email(message=body)
---
+++ simple_ai.py
@@ -0,0 +1,14 @@
+from langchain.chat_models import init_chat_model
+
+GOOGLE_API_KEY = "AIzaSyDXhS0hCNr7ohiSNElro7WBzVXV6xacw0M"
+
+model = init_chat_model(
+    model="gemini-2.5-flash",
+    model_provider="google-genai",
+    api_key=GOOGLE_API_KEY
+)
+
+response = model.invoke("Hey, how're you today?")
+response_str = response.content
+print(response_str)
+
